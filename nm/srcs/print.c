db96ddf8b929bf1628460977a36c7193780c297a
Cyril Raffatelli <craffate@student.42.fr>
2020-08-11 03:04:31 +0200

    Update print functions

--- nm/srcs/print.c
+++ nm/srcs/print.c
@@ -6,7 +6,7 @@
 /*   By: craffate <craffate@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/11 02:31:38 by craffate          #+#    #+#             */
-/*   Updated: 2020/08/11 02:33:02 by craffate         ###   ########.fr       */
+/*   Updated: 2020/08/11 03:04:08 by craffate         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,43 @@ static void				print_symbol_array(char **s_arr)
 }
 
 /*
+** Makes a buffer out of the symbol information and return it.
+*/
+
+static char				*get_symbol_buffer(t_symbol sym)
+{
+	char				*ret;
+	char				type[4];
+
+	if (!(ret = (char *)malloc(sizeof(char) * 1024)))
+		ret = NULL;
+	else
+	{
+		type[0] = ' ';
+		type[1] = sym.type;
+		type[2] = ' ';
+		type[3] = 0;
+		ft_bzero(ret, 1024);
+		if (ft_strlen(sym.value) > 1 && *sym.value != '0')
+		{
+			ft_strncpy(ret, "0000000000000000", 16 - ft_strlen(sym.value));
+			ft_strcat(ret, sym.value);
+		}
+		else
+			ft_strncpy(ret, "                ", 16);
+		ft_strcat(ret, type);
+		ft_strcat(ret, sym.name);
+		ft_strcat(ret, "\n");
+	}
+	return (ret);
+}
+
+/*
 ** Prints a symbol list in alphabetical order.
 */
 
 void					print_symbols_sorted(t_symbol *s_lst)
 {
-	char				buf[256];
-	char				type_buf[4];
 	t_symbol			*s_idx;
 	char				**s_arr;
 	size_t				s_arr_si;
@@ -44,24 +74,9 @@ void					print_symbols_sorted(t_symbol *s_lst)
 	if (!(s_arr = init_symbols_buffer(s_lst)))
 		return ;
 	s_arr_si = 0;
-	type_buf[0] = ' ';
-	type_buf[2] = ' ';
-	type_buf[3] = 0;
 	while (s_idx)
 	{
-		ft_bzero(buf, 256);
-		type_buf[1] = s_idx->type;
-		if (ft_strlen(s_idx->value) > 1 && *s_idx->value != '0')
-		{
-			ft_strncpy(buf, "0000000000000000", 16 - ft_strlen(s_idx->value));
-			ft_strcat(buf, s_idx->value);
-		}
-		else
-			ft_strncpy(buf, "                ", 16);
-		ft_strcat(buf, type_buf);
-		ft_strcat(buf, s_idx->name);
-		ft_strcat(buf, "\n");
-		s_arr[s_arr_si++] = ft_strdup(buf);
+		s_arr[s_arr_si++] = get_symbol_buffer(*s_idx);
 		s_idx = s_idx->next;
 	}
 	bubblesort_symbols(s_arr, s_arr_si);
@@ -74,29 +89,18 @@ void					print_symbols_sorted(t_symbol *s_lst)
 
 void					print_symbols(t_symbol *s_lst)
 {
-	char				buf[256];
-	char				type_buf[4];
 	t_symbol			*s_idx;
+	char				**s_arr;
+	size_t				s_arr_si;
 
 	s_idx = s_lst;
-	type_buf[0] = ' ';
-	type_buf[2] = ' ';
-	type_buf[3] = 0;
+	if (!(s_arr = init_symbols_buffer(s_lst)))
+		return ;
+	s_arr_si = 0;
 	while (s_idx)
 	{
-		ft_bzero(buf, 256);
-		type_buf[1] = s_idx->type;
-		if (ft_strlen(s_idx->value) > 1 && *s_idx->value != '0')
-		{
-			ft_strncpy(buf, "0000000000000000", 16 - ft_strlen(s_idx->value));
-			ft_strcat(buf, s_idx->value);
-		}
-		else
-			ft_strncpy(buf, "                ", 16);
-		ft_strcat(buf, type_buf);
-		ft_strcat(buf, s_idx->name);
-		ft_strcat(buf, "\n");
-		ft_putstr(buf);
+		s_arr[s_arr_si++] = get_symbol_buffer(*s_idx);
 		s_idx = s_idx->next;
 	}
+	print_symbol_array(s_arr);
 }
